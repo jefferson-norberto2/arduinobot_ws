6c8101f64519d7dd3218b4c849bc996bf6d657f9
jefferson-norberto2 <jmn@cin.ufpe.br>
2025-07-25 15:07:02 -0300

    FEAT: Moviet2 files

--- arduinobot_py_examples/setup.py
+++ arduinobot_py_examples/setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-package_name = 'arduino_py_examples'
+package_name = 'arduinobot_py_examples'
 
 setup(
     name=package_name,
@@ -20,11 +20,11 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'simple_publisher = arduino_py_examples.simple_publisher:main',
-            'simple_subscriber = arduino_py_examples.simple_subscriber:main',
-            'simple_parameter = arduino_py_examples.simple_parameter:main',
-            'simple_service_server = arduino_py_examples.simple_service_server:main',
-            'simple_service_client = arduino_py_examples.simple_service_client:main',
+            'simple_publisher = arduinobot_py_examples.simple_publisher:main',
+            'simple_subscriber = arduinobot_py_examples.simple_subscriber:main',
+            'simple_parameter = arduinobot_py_examples.simple_parameter:main',
+            'simple_service_server = arduinobot_py_examples.simple_service_server:main',
+            'simple_service_client = arduinobot_py_examples.simple_service_client:main',
         ],
     },
 )
