33b174c59cbfe9ed2d3e18a99b5d594e40e87ac6
jefferson-norberto2 <jmn@cin.ufpe.br>
2025-07-26 14:14:45 -0300

    REFACTOR: Add type message to linter

--- arduinobot_utils/arduinobot_utils/angle_conversion.py
+++ arduinobot_utils/arduinobot_utils/angle_conversion.py
@@ -13,13 +13,13 @@ class AnglesConverter(Node):
 
         self.get_logger().info('Angle conversion services ready')
 
-    def euler_to_quaternion_calback(self, req, res):
+    def euler_to_quaternion_calback(self, req: EulerToQuaternion.Request, res: QuaternionToEuler.Response):
         self.get_logger().info(f'Request to convert Euler angles roll: {req.roll}, pitch: {req.pitch}, yaw: {req.yaw}, into a Quaternion')
         (res.x, res.y, res.z, res.w) = quaternion_from_euler(req.roll, req.pitch, req.yaw)
         self.get_logger().info(f'Corresponding quartenion x:{res.x}, y:{res.y}, z{res.z}, w:{res.w}')
         return res
 
-    def quaternion_to_euler_calback(self, req, res):
+    def quaternion_to_euler_calback(self, req: QuaternionToEuler.Request, res: QuaternionToEuler.Response):
         self.get_logger().info(f'Request to convert Quaternion x: {req.x}, y: {req.y}, z: {req.z}, w: {req.w} into a Euler')
         (res.roll, res.pitch, res.yaw) = euler_from_quaternion([req.x, req.y, req.z, req.w])
         self.get_logger().info(f'Corresponding Euler angles roll:{res.roll}, pitch:{res.pitch}, yaw:{res.yaw}')
