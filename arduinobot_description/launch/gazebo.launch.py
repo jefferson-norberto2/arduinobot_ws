63da48c42c1eb431ddfb3516c2a64ec8ffd8b820
jefferson-norberto2 <jmn@cin.ufpe.br>
2025-07-28 09:17:09 -0300

    FIX: Using moveit controller

--- arduinobot_description/launch/gazebo.launch.py
+++ arduinobot_description/launch/gazebo.launch.py
@@ -1,71 +1,69 @@
-from launch import LaunchDescription
-from pathlib import Path
-from launch_ros.actions import Node
-from launch.actions import DeclareLaunchArgument, SetEnvironmentVariable, IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch_ros.parameter_descriptions import ParameterValue
-from launch.substitutions import Command, LaunchConfiguration
 import os
+from pathlib import Path
 from ament_index_python.packages import get_package_share_directory
 
-def generate_launch_description():
-    # Find the pkg directory path
-    arduino_bot_description_dir = get_package_share_directory("arduinobot_description")
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, SetEnvironmentVariable
+from launch.substitutions import Command, LaunchConfiguration
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 
-    model_arg = DeclareLaunchArgument(
-        name="model", 
-        default_value=os.path.join(arduino_bot_description_dir, 
-                                    "urdf", 
-                                    "arduinobot.urdf.xacro"),
-        description="Absolute path to the robot URDF file"
-        )
-    
-    # Setting args to gazebo
+from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
+
+
+def generate_launch_description():
+    arduinobot_description = get_package_share_directory("arduinobot_description")
+
+    model_arg = DeclareLaunchArgument(name="model", default_value=os.path.join(
+                                        arduinobot_description, "urdf", "arduinobot.urdf.xacro"
+                                        ),
+                                      description="Absolute path to robot urdf file"
+    )
+
     gazebo_resource_path = SetEnvironmentVariable(
         name="GZ_SIM_RESOURCE_PATH",
         value=[
-            str(Path(arduino_bot_description_dir).parent.resolve())
-        ]
-    )
-
+            str(Path(arduinobot_description).parent.resolve())
+            ]
+        )
+    
     ros_distro = os.environ["ROS_DISTRO"]
     is_ignition = "True" if ros_distro == "humble" else "False"
     physics_engine = "" if ros_distro == "humble" else "--physics-engine gz-physics-bullet-featherstone-plugin"
 
-    # Transform declared launch on args
-    robot_description = ParameterValue(Command(["xacro ", LaunchConfiguration("model"), " is_ignition:=", is_ignition]), value_type=str)
+    robot_description = ParameterValue(Command([
+            "xacro ",
+            LaunchConfiguration("model"),
+            " is_ignition:=",
+            is_ignition
+        ]),
+        value_type=str
+    )
 
-    # Node to publish robot informations
-    robot_state_publisher = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
+    robot_state_publisher_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
         parameters=[{"robot_description": robot_description,
                      "use_sim_time": True}]
     )
 
-    # Start gazebo using python descritpion
     gazebo = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([
-            os.path.join(
-                get_package_share_directory("ros_gz_sim"),
-                "launch"
-            ), "/gz_sim.launch.py"
-        ]),
-        launch_arguments=[
-            ("gz_args", ["-v 4 -r empty.sdf", physics_engine])
-        ]
-    )
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory("ros_gz_sim"), "launch"), "/gz_sim.launch.py"]),
+                launch_arguments=[
+                    ("gz_args", [" -v 4 -r empty.sdf ", physics_engine]
+                    )
+                ]
+             )
 
-
-    # Spaw robot on gazebo world
-    gz_spaw_entity = Node(
+    gz_spawn_entity = Node(
         package="ros_gz_sim",
         executable="create",
         output="screen",
-        arguments=["-topic", "robot_description", "-name", "arduinobot"]
+        arguments=["-topic", "robot_description",
+                   "-name", "arduinobot"],
     )
 
-    # Call the bridge to use gazebo and ros2
     gz_ros2_bridge = Node(
         package="ros_gz_bridge",
         executable="parameter_bridge",
@@ -75,13 +73,12 @@ def generate_launch_description():
             "/camera_info@sensor_msgs/msg/CameraInfo[gz.msgs.CameraInfo",
         ]
     )
-    
-    # Put the rigth sequence to launch robot
+
     return LaunchDescription([
         model_arg,
         gazebo_resource_path,
-        robot_state_publisher,
+        robot_state_publisher_node,
         gazebo,
-        gz_spaw_entity,
+        gz_spawn_entity,
         gz_ros2_bridge
     ])
