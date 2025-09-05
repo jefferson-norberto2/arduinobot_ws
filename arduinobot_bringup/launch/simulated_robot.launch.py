7af37c040ad8b92595bc2d278b545aabf35f7586
jefferson-norberto2 <jmn@cin.ufpe.br>
2025-09-05 10:20:19 -0300

    FEAT: Arduino Firmware

--- arduinobot_bringup/launch/simulated_robot.launch.py
+++ arduinobot_bringup/launch/simulated_robot.launch.py
@@ -31,17 +31,17 @@ def generate_launch_description():
             launch_arguments={"is_sim": "True"}.items()
         )
     
-    remote_interface = IncludeLaunchDescription(
-            os.path.join(
-                get_package_share_directory("arduinobot_remote"),
-                "launch",
-                "remote_interface.launch.py"
-            ),
-        )
+    # remote_interface = IncludeLaunchDescription(
+    #         os.path.join(
+    #             get_package_share_directory("arduinobot_remote"),
+    #             "launch",
+    #             "remote_interface.launch.py"
+    #         ),
+    #     )
     
     return LaunchDescription([
         gazebo,
         controller,
         moveit,
-        remote_interface,
+        # remote_interface,
     ])
